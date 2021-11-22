8046153159fb45e37eae2e43c18443b6c497ea33
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-22 12:23:23 +0530

    Update sample_config.py

--- LegendRobot/sample_config.py
+++ LegendRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/PglRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/LegendRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
