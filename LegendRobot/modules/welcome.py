51ec0e720c6c9e82f69265e129f930e8defe9faf
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-22 12:54:01 +0530

    Update welcome.py

--- LegendRobot/modules/welcome.py
+++ LegendRobot/modules/welcome.py
@@ -5,9 +5,9 @@ import time
 from functools import partial
 from contextlib import suppress
 
-import PglRobot.modules.sql.welcome_sql as sql
-import PglRobot
-from PglRobot import (
+impor LegendRobot.modules.sql.welcome_sql as sql
+import LegendRobot
+from LegendRobot import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -19,18 +19,18 @@ from PglRobot import (
     dispatcher,
     JOIN_LOGGER,
 )
-from PglRobot.modules.helper_funcs.chat_status import (
+from LegendRobot.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from PglRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from PglRobot.modules.helper_funcs.msg_types import get_welcome_type
-from PglRobot.modules.helper_funcs.string_handling import (
+from LegendRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from LegendRobot.modules.helper_funcs.msg_types import get_welcome_type
+from LegendRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from PglRobot.modules.log_channel import loggable
-from PglRobot.modules.sql.global_bans_sql import is_user_gbanned
+from LegendRobot.modules.log_channel import loggable
+from LegendRobot.modules.sql.global_bans_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
@@ -267,7 +267,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome yourself
             elif new_mem.id == bot.id:
                 creator = None
-                if not PglRobot.ALLOW_CHATS:
+                if not LegendRobot.ALLOW_CHATS:
                     with suppress(BadRequest):
                         update.effective_message.reply_text(
                             f"Groups are disabled for {bot.first_name}, I'm outta here."
