c6db021ad4ce9bf96017bda788a814f7fca405f4
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-22 12:56:09 +0530

    Update zombies.py

--- LegendRobot/modules/zombies.py
+++ LegendRobot/modules/zombies.py
@@ -8,7 +8,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from PglRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from LegendRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
